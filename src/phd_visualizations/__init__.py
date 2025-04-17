ba2057ea40d44d1816949016f70ea59be82efb89
juan11iguel <juan11iguel@gmail.com>
2025-04-17 09:01:46 +0200

    Improved save_figure path handling

--- src/phd_visualizations/__init__.py
+++ src/phd_visualizations/__init__.py
@@ -25,14 +25,18 @@ def save_figure(fig: go.Figure, figure_name: str, figure_path: str | Path | list
         height = fig.layout.height
 
     for path in figure_path:
+        path = Path(path)
+        
         for fmt in formats:
             if fmt not in ['eps', 'png', 'svg', 'html']:
                 raise ValueError(f'Format {fmt} not supported')
-
+            
+            output_path = (path/figure_name).with_suffix(f".{fmt}")
+            
             if fmt == 'html':
-                fig.write_html(f'{path}/{figure_name}.{fmt}', include_plotlyjs='cdn', full_html=False)
+                fig.write_html(output_path, include_plotlyjs='cdn', full_html=False)
             else:
-                fig.write_image(f'{path}/{figure_name}.{fmt}', format=fmt, width=width, height=height, scale=scale)
+                fig.write_image(output_path, format=fmt, width=width, height=height, scale=scale)
                 # plt.savefig(f'{path}/{figure_name}.{fmt}', format=fmt)
 
-            logger.info(f'Figure saved in {figure_path}/{figure_name}.{fmt}')
+            logger.info(f'Figure saved in {output_path}')
